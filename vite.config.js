c19cf84e24888f3e92c5f9042b4736fa278d9bdc
DANIEL ALEJANDRO RUIZ POLI <danielruiz368@gmail.com>
2025-03-01 11:04:30 +0100

    Finalized the web page.
    Completed the latest updates and refinements.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,27 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     base: "./",
+    build: {
+        minify: 'terser',
+        cssMinify: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+            },
+            output: {
+                manualChunks: {
+                    vendor: ['src/main.js'],
+                },
+            },
+        },
+    },
+    css: {
+        devSourcemap: true,
+    },
+    server: {
+        open: true,
+        host: true,
+    },
 });
